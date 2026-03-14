35cef9315b304a720a9aaefd6c80e08add44686e
AhmedOmar2003 <ahmedomarahlawy22@gmail.com>
2026-03-14 22:00:29 +0200

    fix ts error

--- worker/index.ts
+++ worker/index.ts
@@ -3,8 +3,6 @@
 // To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
 // https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
 
-declare let self: ServiceWorkerGlobalScope;
-
 self.addEventListener('push', (event: any) => {
   const data = JSON.parse(event?.data?.text() || '{}');
 
